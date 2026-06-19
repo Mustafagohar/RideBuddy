3167fd33d58ef8d1dd73c85fb8f89cda7d759e14
Mustafagohar <125015897+Mustafagohar@users.noreply.github.com>
2026-06-19 12:47:41 +0530

    Update server.js

--- RideBuddy/backend/server.js
+++ RideBuddy/backend/server.js
@@ -3,33 +3,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
-import adminRoute from './routes/adminRoute.js'
-import vendorRoute from './routes/venderRoute.js'
-import cors from 'cors'
+import adminRoute from "./routes/adminRoute.js";
+import vendorRoute from "./routes/venderRoute.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { cloudinaryConfig } from "./utils/cloudinaryConfig.js";
 
+dotenv.config();
 
 const App = express();
 
-
 App.use(express.json());
-App.use(cookieParser())
-
-
-dotenv.config();
-const port = 3000;
-
-mongoose
-  .connect(process.env.mongo_uri)
-  .then(console.log("connected"))
-  .catch((error) => console.error(error));
-
-  
-
-App.listen(port, () => {
-  console.log("server listening !");
-});
+App.use(cookieParser());
 
 App.use(
   cors({
@@ -39,25 +24,41 @@ App.use(
   })
 );
 
+mongoose
+  .connect(process.env.mongo_uri)
+  .then(() => console.log("connected"))
+  .catch((error) => console.error(error));
 
-App.use('*', cloudinaryConfig);
+App.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 
-// App.get('/*', (req, res) => res.sendFile(resolve(__dirname, '../public/index.html')));
+App.get("/test", (req, res) => {
+  res.send("Test Route Working");
+});
 
+console.log("AUTH ROUTE REGISTERING");
+
+App.use("*", cloudinaryConfig);
 
 App.use("/api/user", userRoute);
 App.use("/api/auth", authRoute);
-App.use("/api/admin",adminRoute);
-App.use("/api/vendor",vendorRoute)
-
-
+App.use("/api/admin", adminRoute);
+App.use("/api/vendor", vendorRoute);
 
 App.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "internal server error";
+
   return res.status(statusCode).json({
-    succes: false,
+    success: false,
     message,
     statusCode,
   });
+});
+
+const port = process.env.PORT || 3000;
+
+App.listen(port, () => {
+  console.log(`server listening on ${port}`);
 });
